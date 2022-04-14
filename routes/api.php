564572321a26d6677d7dbf06b9763aa4c64e8c66
Guilherme <guilherme.vgp@outlook.com>
2022-04-14 18:17:47 -0300

    Criando os Controllers

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BilletController;
+use App\Http\Controllers\DocController;
+use App\Http\Controllers\FoundAndLostController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\UnitController;
+use App\Http\Controllers\WallController;
+use App\Http\Controllers\WarningController;
 
 /*
 |--------------------------------------------------------------------------
